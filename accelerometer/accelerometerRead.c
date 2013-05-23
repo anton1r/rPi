f600fda2027aecbe1b6d37d20b089dab8baa3cf1
anton1r <anton1r@gmail.com>
2013-05-23 20:32:50 +0000

    MMA8452Q with working example in c, outputting values (Must check values are as expected though)

--- accelerometer/accelerometerRead.c
+++ accelerometer/accelerometerRead.c
@@ -6,8 +6,8 @@
  */
 
 #include <bcm2835.h> // Used for I2C
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 
 // The SparkFun breakout board defaults to 1, set to 0 if SA0 jumper on the bottom of the board is set
 char MMA8452_ADDRESS = 0x1D;  // 0x1D if SA0 is high, 0x1C if low
@@ -20,23 +20,36 @@ char CTRL_REG1 = 0x2A;
 
 int GSCALE = 2; // Sets full-scale range to +/-2, 4, or 8g. Used to calc real g values.
 
+void sleep()
+{
+    double delay;
+
+    time_t start;
+    time(&start);
+    do{
+      time_t now;
+      time(&now);
+
+      delay = difftime(now, start);
+
+    }while(delay<1);
+}
+
 // Writes a single byte (dataToWrite) into addressToWrite
 void writeRegister(char addressToWrite, char dataToWrite)
 {
-	char buf[2] = {addressToWrite, dataToWrite};
-	bcm2835_i2c_write(buf, 2);
+    char buf[2] = {addressToWrite, dataToWrite};
+    bcm2835_i2c_write(buf, 2);
 
 }
 
 // Read bytesToRead sequentially, starting at addressToRead into the dest byte array
-void readRegisters(char addressToRead, int bytesToRead, byte * dest)
+void readRegisters(char addressToRead, int bytesToRead, char* dest)
 {
     //Set Address to read
-    char buf[1] = {addressToWrite};
-	bcm2835_i2c_write(buf, 1);
+    bcm2835_i2c_write(&addressToRead, 1);
 
     //Get reading
-    char dest[bytesToRead];
     bcm2835_i2c_read_register_rs(&addressToRead,dest,bytesToRead);
 
 }
@@ -59,7 +72,16 @@ void MMA8452Active()
 
   char buff[2];
   bcm2835_i2c_read_register_rs(&CTRL_REG1,buff,2);
-  printf("Returned active value %X\n",*buff);
+  if (*buff == 1) // WHO_AM_I should always be 0x2A
+  {
+    printf("MMA8452Q is Active \n");
+
+  }
+  else
+  {
+    printf("MMA8452Q is Inactive \n");
+    while(1) ; // Loop forever if communication doesn't happen
+  }
 }
 
 void readAccelData(int *destination)
@@ -69,7 +91,8 @@ void readAccelData(int *destination)
   readRegisters(OUT_X_MSB, 6, rawData);  // Read the six raw data registers into data array
 
   // Loop to calculate 12-bit ADC and g value for each axis
-  for(int i = 0; i < 3 ; i++)
+  int i;
+  for(i=0;i<3;i++)
   {
     int gCount = (rawData[i*2] << 8) | rawData[(i*2)+1];  //Combine the two 8 bit registers into one 12-bit number
     gCount >>= 4; //The registers are left align, here we right align the 12-bit integer
@@ -92,20 +115,24 @@ void loop()
 
   // Now we'll calculate the accleration value into actual g's
   float accelG[3];  // Stores the real accel value in g's
-  for (int i = 0 ; i < 3 ; i++)
+  int i;
+  for(i=0;i<3;i++)
   {
     accelG[i] = (float) accelCount[i] / ((1<<12)/(2*GSCALE));  // get actual g value, this depends on scale being set
   }
 
   // Print out values
-  for (int i = 0 ; i < 3 ; i++)
+  int j;
+  for(j=0;j<3;j++)
   {
-    printf(accelG[i], 4);  // Print g values
+    //printf(accelG[j], 4);  // Print g values
+    printf("%G",accelG[j]);
     printf("\t");  // tabs in between axes
   }
-  printf();
+  printf("\n");
 
-  sleep(100);  // Delay here for visibility
+//Attempt a delay
+  sleep();
 }
 
 
@@ -114,8 +141,8 @@ void loop()
 // http://www.freescale.com/webapp/sps/site/prod_summary.jsp?code=MMA8452Q
 void initMMA8452()
 {
-	char buf[1];
-	bcm2835_i2c_read_register_rs(&WHO_AM_I,buf,1);
+    char buf[1];
+    bcm2835_i2c_read_register_rs(&WHO_AM_I,buf,1);
 
   if (*buf == CTRL_REG1) // WHO_AM_I should always be 0x2A
   {
@@ -153,6 +180,8 @@ int main(int argc, char **argv)
 
   initMMA8452(); //Test and intialize the MMA8452
 
-  loop();
+while(1){
+    loop();
+  }
   return 0;
 }
