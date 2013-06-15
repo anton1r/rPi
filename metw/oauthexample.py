e2bf874ece5b5c6d6c2bf9c72e19d152dc9dfa1e
anton1r <anton1r@gmail.com>
2013-06-15 21:32:37 +0000

    adding python twitter bits2

--- metw/oauthexample.py
+++ metw/oauthexample.py
@@ -4,9 +4,6 @@ import os
 MY_TWITTER_CREDS = os.path.expanduser('~/.my_app_credentials')
 MY_TWITTER_CREDS2 = os.path.expanduser('~/.my_app_credentials2')
 
-CONSUMER_KEY = "4q99OGiOKxdyyQ3QliP1g"
-CONSUMER_SECRET = "E7HvqhRqXptUiCMbML1LUh0F9HS6kVj48cq2Hb9Bwc"
-
 if not os.path.exists(MY_TWITTER_CREDS):
    oauth_dance("metw", CONSUMER_KEY, CONSUMER_SECRET,
                 MY_TWITTER_CREDS)
