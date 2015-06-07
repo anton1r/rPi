9e681f6c73cfe94ea4d7243513696973c4521ae4
anton1r <anton1r@gmail.com>
2015-06-07 08:31:17 +0100

    Jaspered bustimes

--- PycharmProjects/BusTimes/BusTimesCore.py
+++ PycharmProjects/BusTimes/BusTimesCore.py
@@ -1,11 +1,28 @@
 from lxml import html
 import requests
+import sys
+import time
+import re
 
-page = requests.get('http://www.mybustracker.co.uk/?module=mobile&mode=1&busStopCode=36232457&subBusStop=Display+Departures')
+WORDS = ["BUS"]
 
-clean = page.text.replace("&nbsp","")
-tree = html.fromstring(clean);
+PRIORITY = 1
 
-times = tree.xpath('//td[@class="time"]//a/text()')
-print(times)
+def handle(text, mic, profile):
 
+    page = requests.get('http://www.mybustracker.co.uk/?module=mobile&mode=1&busStopCode=36232457&subBusStop=Display+Departures')
+
+    clean = page.text.replace("&nbsp","")
+    tree = html.fromstring(clean)
+
+    times = tree.xpath('//td[@class="time"]//a/text()')
+
+    if(times[0] == "DUE"):
+        str = "The next bus is DUE"
+    else :
+        str =  "The next bus is in "+ times[0]+" minutes"
+
+    mic.say(str + " and then in "+times[1]+" minutes")
+
+def isValid(text):
+    return bool(re.search(r'\Bus\b', text, re.IGNORECASE))
